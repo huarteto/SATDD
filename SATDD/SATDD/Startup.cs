ad673314c8a6c951ddec7e4b3571373edea9c84f
Alvaro Huarte <alvaroSATD@hotmail.com>
2018-11-27 18:27:28 +0100

    Cambios en Controller Vallue

---
+++ SATDD/SATDD/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SATDD.Startup))]
+
+namespace SATDD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
